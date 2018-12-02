917c94f43739099f75ea1334a0b5a99977fee0e1
Lucas Silveira Vieira <lucassilveira@macbook-air-de-lucas.local>
2018-12-01 23:53:47 -0200

    função porcentagem

--- Teste/ViewController.swift
+++ Teste/ViewController.swift
@@ -15,14 +15,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var porcao_2: UITextField!
     @IBOutlet weak var quantidade_x: UILabel!
     
+    @IBOutlet weak var valor: UITextField!
+    @IBOutlet weak var porcentagem: UITextField!
+    @IBOutlet weak var desconto: UILabel!
+    @IBOutlet weak var valor_de_porcentagem: UILabel!
+    
     @IBAction func regra_3(){
         let a:Double = Double(quantidade_1.text!)!
         let b:Double = Double(porcao_1.text!)!
         let c:Double = Double(porcao_2.text!)!
         let x:Double = (a * c)/b
-        quantidade_x.text = String(format:"%f", x)
+        quantidade_x.text = String(x)
+        
     
     }
+    
+    @IBAction func calcular_porcentagem() {
+        let a:Double = Double(valor.text!)!
+        let b:Double = (Double(porcentagem.text!)!)/100
+        let c:Double = b * a
+        desconto.text = String(c)
+        valor_de_porcentagem.text = String(a - c)
+    }
+    
 
 
 }
